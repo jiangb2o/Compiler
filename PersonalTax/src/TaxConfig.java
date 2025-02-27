4785588a3f29520228aea440810ad820e0042c5a
jiangbo <jiangb2o@foxmail.com>
2025-02-27 23:59:11 +0800

    Use ArrayList to replace array

--- PersonalTax/src/TaxConfig.java
+++ PersonalTax/src/TaxConfig.java
@@ -1,5 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
-
 import static java.lang.Math.min;
 
 /**
@@ -8,18 +9,18 @@ import static java.lang.Math.min;
 public class TaxConfig {
     private int levelCount;
     private int taxThreshold;
-    private Pair<Float, Float>[] taxRates;
+    private List<Pair<Float, Float>> taxRates;
 
     public TaxConfig() {
         this.levelCount = 5;
         this.taxThreshold = 1600;
-        this.taxRates = new Pair[this.levelCount];
+        this.taxRates = new ArrayList<>();
 
-        this.taxRates[0] = new Pair<>(500f, 0.05f);
-        this.taxRates[1] = new Pair<>(2000f, 0.1f);
-        this.taxRates[2] = new Pair<>(5000f, 0.15f);
-        this.taxRates[3] = new Pair<>(20000f, 0.2f);
-        this.taxRates[4] = new Pair<>(Float.MAX_VALUE, 0.25f);
+        this.taxRates.add(new Pair<>(500f, 0.05f));
+        this.taxRates.add(new Pair<>(2000f, 0.1f));
+        this.taxRates.add(new Pair<>(5000f, 0.15f));
+        this.taxRates.add(new Pair<>(20000f, 0.2f));
+        this.taxRates.add(new Pair<>(Float.MAX_VALUE, 0.25f));
     }
 
     /**
@@ -34,7 +35,7 @@ public class TaxConfig {
         System.out.print("Input level count:");
         levelCount = scanner.nextInt();
 
-        taxRates = new Pair[levelCount];
+        taxRates.clear();
         for (int i = 0; i < levelCount; i++) {
             float taxableIncome = Float.MAX_VALUE;
             float rate;
@@ -46,7 +47,7 @@ public class TaxConfig {
                 taxableIncome = scanner.nextFloat();
                 rate = scanner.nextFloat();
             }
-            taxRates[i] = new Pair<>(taxableIncome, rate);
+            taxRates.add(new Pair<>(taxableIncome, rate));
         }
     }
 
@@ -58,22 +59,26 @@ public class TaxConfig {
     public void calculateTax(float income) {
         float totalTax = 0;
         float taxableIncome = income - taxThreshold;
-        if(taxableIncome <= 0) return ;
+        if(taxableIncome <= 0) {
+            System.out.println("Tax threshold not reached.");
+            return ;
+        }
 
+        System.out.format("Total Taxable is: %.2f\n", taxableIncome);
         for(int i = 0; i < levelCount; ++i) {
             float levelTaxable;
             float levelTax;
             if(i == 0) {
-                levelTaxable = min(taxableIncome, taxRates[i].first);
+                levelTaxable = min(taxableIncome, taxRates.get(i).first);
             } else {
-                levelTaxable = min(taxableIncome, taxRates[i].first) - taxRates[i-1].first;
+                levelTaxable = min(taxableIncome, taxRates.get(i).first) - taxRates.get(i-1).first;
             }
             if(levelTaxable <= 0) break;
 
-            levelTax = levelTaxable * taxRates[i].second;
+            levelTax = levelTaxable * taxRates.get(i).second;
             totalTax += levelTax;
-            System.out.format("Level %d : taxable is %.2f tax is %.2f * %.2f = %.2f\n",
-                    i + 1, levelTaxable, levelTaxable, taxRates[i].second, levelTax);
+            System.out.format("Level %2d : taxable is %8.2f tax is %8.2f * %.2f = %8.2f\n",
+                    i + 1, levelTaxable, levelTaxable, taxRates.get(i).second, levelTax);
         }
         System.out.format("Total Tax : %.2f\n", totalTax);
     }
@@ -87,10 +92,10 @@ public class TaxConfig {
         System.out.println("\tTax Level Count: " + levelCount);
         for(int i = 0; i < levelCount; ++i) {
             System.out.print("\tLevel " + (i + 1) + ": ");
-            String ceil = i == levelCount - 1 ? "  inf" : String.format("%5.0f",taxRates[i].first);
-            String floor = i == 0 ? "    0" : String.format("%5.0f", taxRates[i-1].first);
+            String ceil = i == levelCount - 1 ? "  inf" : String.format("%5.0f",taxRates.get(i).first);
+            String floor = i == 0 ? "    0" : String.format("%5.0f", taxRates.get(i-1).first);
 
-            System.out.println("[" + floor + ',' + ceil + "]\t" + taxRates[i].second);
+            System.out.println("[" + floor + ',' + ceil + "]\t" + taxRates.get(i).second);
         }
     }
 
